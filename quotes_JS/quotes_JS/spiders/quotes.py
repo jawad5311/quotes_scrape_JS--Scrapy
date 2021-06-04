fd6a248b52d12140743c19cbeb0f74bfa7129434
Jawad <muhammadjawad5311@gmail.com>
2021-06-04 14:53:07 +0500

    request made and html markup recieved successfully

--- quotes_JS/quotes_JS/spiders/quotes.py
+++ quotes_JS/quotes_JS/spiders/quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class QuotesSpider(scrapy.Spider):
@@ -6,5 +7,29 @@ class QuotesSpider(scrapy.Spider):
     allowed_domains = ['quotes.toscrape.com/js']
     start_urls = ['http://quotes.toscrape.com/js/']
 
+    script = """
+    function main(splash, args)
+  
+        splash.private_mode_enabled = false
+        
+        assert(splash:go(args.url))
+        assert(splash:wait(1))
+        splash:set_viewport_full()
+        
+        return splash:html()
+  
+    end
+    """
+
+    def start_requests(self):
+        yield SplashRequest(
+            url='https://quotes.toscrape.com/js',
+            callback=self.parse,
+            endpoint='execute',
+            args={
+                'lua_source': self.script
+            }
+        )
+
     def parse(self, response):
-        pass
+        print(response.body)
